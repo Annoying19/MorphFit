b10295e4e2d3b2bc224278b576768803119d24f6
Annoying19 <josephsalenga03@gmail.com>
2025-04-03 10:26:05 +0800

    Initial commit with Dockerfile and backend code

--- app/main.py
+++ app/main.py
@@ -492,4 +492,5 @@ def remove_outfit_by_id():
     return jsonify({'message': 'Saved outfit removed successfully'}), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
